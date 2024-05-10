cc6d6305e4393421ae7cbdae8152cc7188f4929f
rei <j6m9nw0k@duck.com>
2024-05-10 00:32:04 -0700

    Fight.fs and Fighter.fs

---
+++ Projects/Nugen/Fight.fs
@@ -0,0 +1,45 @@
+﻿namespace Nugen
+open Nu
+
+type FightMessage =
+    | Nop
+    interface Message
+    
+type FightCommand =
+    | Nop
+    interface Command
+
+type PlayerController =
+    | LocalPlayer
+    | AIPlayer
+
+type Player =
+    { Controller: PlayerController
+      fighter: Fighter
+      Score1: int
+      Score2: int
+    }
+    
+    static member make player fighter =
+        { Controller = player
+          fighter = fighter
+          Score1 = 0
+          Score2 = 0
+        }
+
+type Fight =
+    { Player1: Player
+      Player2: Player
+      CameraPosition: Vector2i
+      RoundStartTime : int64 }
+
+    // static member empty = {}
+    //
+    static member initial fighter1AirFile fighter2AirFile =
+        { Player1 = Player.make LocalPlayer (Fighter.initial fighter1AirFile)
+          Player2 = Player.make AIPlayer (Fighter.initial fighter2AirFile)
+          CameraPosition = v2i 0 0
+          RoundStartTime = 0
+        }
+    //
+    // static member make _ _ _ = {}
---
+++ Projects/Nugen/Fighter.fs
@@ -0,0 +1,168 @@
+﻿namespace Nugen
+open Nu
+
+type FighterInput =
+  | Left
+  | Right
+  | Jump
+  | Crouch
+  | Punch
+  | Kick
+
+type InteractionBox =
+  | HurtBox
+  | HitBox
+
+type Collider = Collider of Bounds: Box2i * Kind: InteractionBox
+
+type Facing = Leftward | Rightward
+
+type DamageDescriptor =
+  { points: int }
+
+type FighterMessage =
+  | UpdateInput of FighterInput
+  | TakeDamage of DamageDescriptor
+  
+type FighterCommand =
+  | Nop
+
+type FrameDescriptor = unit
+
+type AnimationFrame =
+  { FrameDescriptor: FrameDescriptor
+    Duration: int
+    Colliders: Collider list
+    Offset: Vector2i }
+
+type Animation =
+    { AnimationFrame : Animation array }
+
+type AnimationType =
+    | JumpAnimation
+
+type Animations =
+    { Animations : Map<AnimationType, Animation> }
+
+type AnimationEndAction =
+  | Repeat
+  | TransitionTo of ActionState
+
+and ActionState =
+  | Standing
+  | Crouching
+  | StandingToCrouching
+  | CrouchingToStanding
+  | Walking
+  | Running
+  | Falling
+  | Fallen
+  | Punching
+  | Kicking
+  | TakingDamage
+  | Defeated
+  member state.frames =
+    match state with
+    | Standing -> []
+    | Crouching -> []
+    | StandingToCrouching -> []
+    | CrouchingToStanding -> []
+    | Walking -> []
+    | Running -> []
+    | Falling -> []
+    | Fallen -> []
+    | Punching -> []
+    | Kicking -> []
+    | TakingDamage -> []
+    | Defeated -> []
+  member state.actionId =
+    match state with
+    | Standing -> 0
+    | Crouching -> 11
+    | StandingToCrouching -> 12
+    | CrouchingToStanding -> 10
+    | Walking -> 20
+    | Running -> 100
+    | Falling -> 5050
+    | Fallen -> 5110
+    | Punching -> 200
+    | Kicking -> 240
+    | TakingDamage -> 120
+    | Defeated -> 5110
+    |> AirFile.ActionId
+  member state.atAnimationEnd =
+    match state with
+    | Standing
+    | Crouching
+    | Walking
+    | Running
+    | Fallen
+    | Defeated -> Repeat
+    | CrouchingToStanding
+    | Punching
+    | Kicking
+    | TakingDamage -> TransitionTo Standing
+    | StandingToCrouching -> TransitionTo Crouching
+    | Falling -> TransitionTo Fallen
+  member state.acceptsInput =
+    match state with
+    | Standing
+    | Crouching
+    | Walking
+    | Running
+    | CrouchingToStanding
+    | StandingToCrouching -> true
+    | Fallen
+    | Defeated
+    | Punching
+    | Kicking
+    | Falling
+    | TakingDamage -> false
+
+type Fighter =
+  { Health: int
+    Action: ActionState
+    ActionStartTime: int64
+    AirFile: AirFile.AirFile
+    Facing: Facing
+    Position: Vector2i }
+  static member initial airFile =
+    { Health = 100
+      Action = Standing
+      ActionStartTime = 0
+      AirFile = airFile
+      Facing = Rightward
+      Position = v2i -100 0 }
+    
+module Fighter =
+    let fighterSpriteAsset (AirFile.ActionId group, frame) =
+      asset<Image> "TenShinHan" $"TenShinHan_%d{group}-%d{frame}"
+    let tempFighterAirFile =
+        System.IO.File.ReadAllLines("Assets/TenShinHan/TenShinHan.air")
+        |> AirFile.parse
+    let rec eatActionFrames (elements: AirFile.AnimationElement list) (frame: int) =
+      match elements with
+      | { Duration = -1 } as element::_ -> element
+      | element::elements ->
+        if element.Duration < frame then
+          eatActionFrames elements (frame - element.Duration)
+        else element
+      | [] -> failwith "No frames for animation"
+    let currentActionElement fighter time =
+      let currentFrame = time - fighter.ActionStartTime
+      let action = fighter.AirFile.Actions[fighter.Action.actionId]
+      let preLoop, loop =
+        List.splitAt action.LoopStartIndex action.Elements
+      let preLoopDuration = preLoop |> List.sumBy (fun element -> if element.Duration < 0 then 0 else element.Duration)
+      let loopDuration =
+          match List.tryLast action.Elements with
+          | Some { Duration = -1 }
+          | None ->
+            System.Int64.MaxValue
+          | _ ->
+            loop |> List.sumBy (fun element -> if element.Duration < 0 then 0 else element.Duration)
+      if currentFrame < preLoopDuration then
+        eatActionFrames preLoop (int currentFrame)
+      else
+        let timeInLoop = (currentFrame - (int64 preLoopDuration)) % loopDuration
+        eatActionFrames loop (int timeInLoop)
